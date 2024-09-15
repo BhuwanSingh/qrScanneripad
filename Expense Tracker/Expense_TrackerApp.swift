55e3f9832fba6492f4b5607959644db1dfbbdd0d
Bhuwan Singh <66213701+BhuwanSingh@users.noreply.github.com>
2024-09-15 21:19:05 +0530

    initial Commit

---
+++ Expense Tracker/Expense_TrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Expense_TrackerApp.swift
+//  Expense Tracker
+//
+//  Created by Bhuwan Singh on 15/09/24.
+//
+
+import SwiftUI
+
+@main
+struct Expense_TrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
